ac5abe3785a095f65c2f67c52a03935e7db38470
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-09-16 10:26:58 +0000

    Task added
    I added the main function and `while` .

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,1 +1,18 @@
 #!usr/bin/python3
+"""Calculates the fewest number of operations needed
+to result in exactly n (H) characters in the file"""
+
+def minOperations(n):
+    """Return the minimum number of operations for n"""
+    if n < 2:
+        return 0
+    
+    oper = 0
+    div = 2
+
+    while n > 1:
+        while n % div == 0:
+            oper += div
+            n //= div
+        div += 1
+    return oper
