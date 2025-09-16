4ec5206d0190fbc2f41a8055dbfe406eb6ba5809
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-09-16 10:46:38 +0000

    fix:  validate PEP8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,13 +3,15 @@
 to result in exactly n (H) characters in the file"""
 
 def minOperations(n):
-    """Return the minimum number of operations for n"""
+    """
+    Return the minimum number of operations for n
+    """
     oper = 0
     div = 2
-    
+
     if n < 2:
         return 0
-    
+
     while n > 1:
         while n % div == 0:
             oper += div
