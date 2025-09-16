7d792c0dd4384e8d81cf94349025f11bef901508
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-09-16 11:51:41 +0000

    add: Test for our module

---
+++ minimum_operations/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 19170307
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 1
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 105350246
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
